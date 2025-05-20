4de72d7398302308759e460df2602ffe068c314e
republic223 <stevensj2@waimate-high.school.nz>
2025-05-20 20:00:30 +1200

    Connecting of the Database via Flask

--- App.py
+++ App.py
@@ -1,13 +1,38 @@
 # importing relevent 3rd party software.
-from flask import Flask
+import sqlite3
+from flask import Flask, g
+
+# Varibles and constants below
 app = Flask(__name__)
+DATABASE = 'Movie_Data.db'
+
+#Conecting Movie_Data.db to program. 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# Function to make querying the database simple.
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv 
 
 #Testing Home Route.
 @app.route("/")
 def Test_Route():
-    return "Hello There"
+    sql = '''SELECT * FROM Shows_Display; '''
+    results = query_db(sql)
+    return results
 
 # Used to run the app in debug mode this will be usful if error occur during devlopment.
 if __name__ == "__main__":
     app.run(debug = True)
-
