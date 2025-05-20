152034035f7fbd07517fdf0a743e08cc0b574f9e
republic223 <stevensj2@waimate-high.school.nz>
2025-05-20 12:20:43 +1200

    App Up and running no functionality

--- App.py
+++ App.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# importing relevent 3rd party software.
+from flask import Flask
+app = Flask(__name__)
+
+#Testing Home Route.
+@app.route("/")
+def Test_Route():
+    return "Hello There"
+
+# Used to run the app in debug mode this will be usful if error occur during devlopment.
+if __name__ == "__main__":
+    app.run(debug = True)
+
