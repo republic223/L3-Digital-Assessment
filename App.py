46c468f3640a95bd5b061639e4cfaa2330a1b796
republic223 <stevensj2@waimate-high.school.nz>
2025-08-26 12:33:53 +1200

    Final PEP8 clean up of code, with debug set to false so error 500 page will show when site is in use

--- App.py
+++ App.py
@@ -3,12 +3,17 @@ import sqlite3
 from flask import Flask, g, render_template, request, flash, session, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 # Varibles and constants below
 app = Flask(__name__)
+
+# Database Veriable set constant 
 DATABASE = 'Movie_Data.db'
 
-# Needed secret key for session to work will make proper key later
-app.config['SECRET_KEY'] = 'cheese'
+
+# Needed secret key for session to work 
+app.config['SECRET_KEY'] = '9b25226542c805a3c8a1b625cc5c2cda3bc35e23'
+
 
 #Conecting Movie_Data.db to program. 
 def get_db():
@@ -17,12 +22,14 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 # Closes database after shutdown of server
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 # Function to make querying the database simple.
 def query_db(query, args=(), one=False, commit=False):
@@ -36,148 +43,170 @@ def query_db(query, args=(), one=False, commit=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv 
 
+
 # Error code Handlers go here
 @app.errorhandler(404)
 def Page_Not_Found(error):
     return render_template('404.html')
 
+
 @app.errorhandler(500)
 def Server_error(error):
     return render_template('500.html')
+
 
 @app.errorhandler(505)
 def HTTP_version_incorrect(error):
     render_template('505.html')
 
-# Used to develop the error pages only temporary will be removed 
-@app.route("/1")
-def error_page():
-    return render_template('505.html')
+
+# # Used to develop the error pages only temporary will be removed 
+# @app.route("/1")
+# def error_page():
+#     return render_template('505.html')
+
 
 #Testing Home Route.
 @app.route("/")
 def Test_Route():
-    sql = '''SELECT show_id, title, Year, Rating_image, Poster_image, Rating
-FROM Shows_display
-JOIN Age_rating ON Shows_display.Rating_id = Age_Rating.Rating_id
-ORDER BY title ASC; '''
+    sql = '''SELECT show_id, title, Year, Rating_image, Poster_image, 
+        Rating
+        FROM Shows_display
+        JOIN Age_rating ON Shows_display.Rating_id = Age_Rating.Rating_id
+        ORDER BY title ASC; '''
     results = query_db(sql)
     return render_template('Home.html', results=results)
 
+
 # Show page route
 @app.route("/Show_page/<int:Show_id>")
-def Show_page(Show_id):
+def Show_Page(Show_id):
     sql = '''SELECT Shows_display.Show_id, title, 
-GROUP_CONCAT(DISTINCT Director.Directors) AS Directors,
-GROUP_CONCAT(DISTINCT Country.Country ) AS Country,
-GROUP_CONCAT(DISTINCT Genre.Genre) AS Genre, 
-actors, Date_added, Year, Rating_image, Duration, Description, Poster_image, Director.Director_id, Country.Country_id, Genre.Genre_id, Rating
-FROM Shows_display
-JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
-left JOIN Show_Director ON Shows_Display.Show_id = Show_Director.Show_id
-left JOIN Director on Show_director.Director_id = Director.Director_id
-left JOIN Show_Country ON Shows_Display.Show_id = Show_Country.Show_id
-left JOIN Country on Show_Country.Country_id = Country.Country_id
-left JOIN Show_Genre ON Shows_Display.Show_id = Show_Genre.Show_id
-left JOIN Genre on Show_Genre.Genre_id = Genre.Genre_id
-WHERE Shows_display.Show_id = ?;'''
+        GROUP_CONCAT(DISTINCT Director.Directors) AS Directors,
+        GROUP_CONCAT(DISTINCT Country.Country ) AS Country,
+        GROUP_CONCAT(DISTINCT Genre.Genre) AS Genre, 
+        actors, Date_added, Year, Rating_image, Duration, Description, 
+        Poster_image, Director.Director_id, Country.Country_id, 
+        Genre.Genre_id, Rating
+        FROM Shows_display
+        JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
+        left JOIN Show_Director ON Shows_Display.Show_id = Show_Director.Show_id
+        left JOIN Director on Show_director.Director_id = Director.Director_id
+        left JOIN Show_Country ON Shows_Display.Show_id = Show_Country.Show_id
+        left JOIN Country on Show_Country.Country_id = Country.Country_id
+        left JOIN Show_Genre ON Shows_Display.Show_id = Show_Genre.Show_id
+        left JOIN Genre on Show_Genre.Genre_id = Genre.Genre_id
+        WHERE Shows_display.Show_id = ?;'''
     results = query_db(sql, args=(Show_id,), one=False)
     return render_template('Show_page.html', results = results) 
+
 
 #TV Shows route
 @app.route("/TV_Shows")
 def TV_Route():
-    sql = '''SELECT show_id, title, Year, Rating_image, Poster_image, Rating, Type
-FROM Shows_display
-JOIN Age_rating ON Shows_display.Rating_id = Age_Rating.Rating_id
-JOIN Movie_types ON Shows_display.Type_id = Movie_types.Type_id
-WHERE Shows_display.Type_id = 2
-ORDER BY title ASC; ''' 
+    sql = '''SELECT show_id, title, Year, Rating_image, Poster_image, 
+        Rating, Type
+        FROM Shows_display
+        JOIN Age_rating ON Shows_display.Rating_id = Age_Rating.Rating_id
+        JOIN Movie_types ON Shows_display.Type_id = Movie_types.Type_id
+        WHERE Shows_display.Type_id = 2
+        ORDER BY title ASC; ''' 
     results = query_db(sql)
     return render_template('TV_Show.html', results= results)
+
 
 #Movies route
 @app.route("/Movies")
 def Movie_Route():
-    sql = '''SELECT show_id, title, Year, Rating_image, Poster_image, Rating, Type
-FROM Shows_display
-JOIN Age_rating ON Shows_display.Rating_id = Age_Rating.Rating_id
-JOIN Movie_types ON Shows_display.Type_id = Movie_types.Type_id
-WHERE Shows_display.Type_id = 1
-ORDER BY title ASC; ''' 
+    sql = '''SELECT show_id, title, Year, Rating_image, Poster_image, 
+        Rating, Type
+        FROM Shows_display
+        JOIN Age_rating ON Shows_display.Rating_id = Age_Rating.Rating_id
+        JOIN Movie_types ON Shows_display.Type_id = Movie_types.Type_id
+        WHERE Shows_display.Type_id = 1
+        ORDER BY title ASC; ''' 
     results = query_db(sql)
     return render_template('Movie.html', results= results)
+
 
 # Director_Select route
 @app.route('/Director_Select')
 def Director_Select():
     sql = '''SELECT Directors, Director_id FROM Director
-    ORDER BY Directors ASC;'''
+        ORDER BY Directors ASC;'''
     results = query_db(sql)
     return render_template('Director_Select.html', results = results)
+
 
 #Directors movies route
 @app.route('/Director/<int:Director_id>')
 def Director_Movies(Director_id):
-    sql = '''SELECT Shows_display.Show_id, title, Year, Rating_image, Poster_image, Rating, Director.Directors
-FROM Shows_display
-JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
-JOIN Show_Director ON Shows_Display.Show_id = Show_Director.Show_id
-JOIN Director on Show_director.Director_id = Director.Director_id
-WHERE Show_Director.Director_id = ?
-; '''
+    sql = '''SELECT Shows_display.Show_id, title, Year, Rating_image, 
+        Poster_image, Rating, Director.Directors
+        FROM Shows_display
+        JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
+        JOIN Show_Director ON Shows_Display.Show_id = Show_Director.Show_id
+        JOIN Director on Show_director.Director_id = Director.Director_id
+        WHERE Show_Director.Director_id = ?; '''
     results = query_db(sql, args=(Director_id,), one= False)
     return render_template('Selection.html', results = results)
+
 
 # Country_Select route
 @app.route('/Country_Select')
 def Country_Select():
     sql= '''SELECT Country, Country_id FROM Country
-            ORDER BY Country ASC;'''
+        ORDER BY Country ASC;'''
     results = query_db(sql)
     return render_template('Country_Select.html', results = results)
+
 
 #Countries movies route
 @app.route('/Country/<int:Country_id>')
 def Country_Movies(Country_id):
     sql = '''SELECT Shows_display.Show_id, title, Year, Rating_image, Poster_image, Rating, Country.Country
-FROM Shows_display
-JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
-JOIN Show_Country ON Shows_Display.Show_id = Show_Country.Show_id
-JOIN Country on Show_Country.Country_id = Country.Country_id
-WHERE Show_Country.Country_id = ?; '''
+        FROM Shows_display
+        JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
+        JOIN Show_Country ON Shows_Display.Show_id = Show_Country.Show_id
+        JOIN Country on Show_Country.Country_id = Country.Country_id
+        WHERE Show_Country.Country_id = ?; '''
     results = query_db(sql, args=(Country_id,), one= False)
     return render_template('Selection.html', results = results)
+
 
 # Genre_Select route
 @app.route('/Genre_Select')
 def Genre_Select():
     sql = '''SELECT Genre, Genre_id FROM Genre
-    ORDER BY Genre ASC;'''
+        ORDER BY Genre ASC;'''
     results = query_db(sql)
     return render_template('Genre_Select.html', results = results)
+
 
 #Countries movies route
 @app.route('/Genre/<int:Genre_id>')
 def Genre_Movies(Genre_id):
     sql = '''SELECT Shows_display.Show_id, title, Year, Rating_image, Poster_image, Rating, Genre.Genre
-FROM Shows_display
-JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
-JOIN Show_Genre ON Shows_Display.Show_id = Show_Genre.Show_id
-JOIN Genre on Show_Genre.Genre_id = Genre.Genre_id
-WHERE Show_Genre.Genre_id = ?; '''
+        FROM Shows_display
+        JOIN Age_rating ON Shows_Display.Rating_id = Age_Rating.Rating_id
+        JOIN Show_Genre ON Shows_Display.Show_id = Show_Genre.Show_id
+        JOIN Genre on Show_Genre.Genre_id = Genre.Genre_id
+        WHERE Show_Genre.Genre_id = ?; '''
     results = query_db(sql, args=(Genre_id,), one= False)
     return render_template('Selection.html', results = results)
+
 
 #Copyright Disclaimer route
 @app.route("/Copyright_Disclaimer")
 def Copyright_Disclaimer():
     return render_template('Copyright.html')
 
+
 # About page route
 @app.route("/About")
 def About():
     return render_template("About.html")
+
 
 # Issues route
 @app.route("/Issues", methods=["GET","POST"])
@@ -186,7 +215,7 @@ def Issues():
         Issue = request.form["Issue"]
         User = session['username']
         sql = '''INSERT INTO Issues (Issue, User_id) VALUES (?,?)'''
-        query_db(sql, args=(Issue, User,), commit=True)   
+        query_db(sql, args=(Issue, User,), commit=True)       
     
     sql = ''' Select User_data.Username, Issue 
             FROM Issues
@@ -194,9 +223,10 @@ def Issues():
     results = query_db(sql)
     return render_template("Issues.html", results = results)
 
+
 # Sign Up route
 @app.route('/Sign_up', methods=["GET","POST"])
-def Sign_up():
+def Sign_Up():
     if request.method == "POST":
         Username = request.form['username']
         password0 = request.form['password0']
@@ -212,18 +242,20 @@ def Sign_up():
             flash("Please ensure both Passwords match")
     return render_template('Sign_up.html')
 
+
 # Check Number Route
 @app.route('/Check_Number')
-def Check_number():
+def Check_Number():
     # Check Number at the momment is the Max Value of the user ID so they can be uniquly identified
     # will need to be improved on if users want to delete data
     sql = '''Select MAX(User_ID) FROM User_Data;'''
     results = query_db(sql)
     return render_template('Check_Num.html', results=results)
 
+
 #Log in route
 @app.route('/Login', methods=["GET","POST"])
-def login():
+def Login():
     if request.method=="POST":
         # find username and password
         username = request.form['username']
@@ -240,7 +272,7 @@ def login():
                     flash("Logged In")
                 # if password does not match program will flash password incorrect
                 else:
-                    flash("Password incorrect")
+                    flash("Password Incorrect")
             # if check number incorrect program will let user know
             else:
                flash("Check Number Incorrect")      
@@ -249,9 +281,10 @@ def login():
             flash("Username does not exist")
     return render_template('Login.html')
 
+
 # sign out route
 @app.route("/Sign_out")
-def Sign_out():
+def Sign_Out():
     # clears session and returns user to the homepage
     session['username'] = None
     return redirect('/')
@@ -259,4 +292,5 @@ def Sign_out():
 
 # Used to run the app in debug mode this will be usful if error occur during devlopment.
 if __name__ == "__main__":
-    app.run(debug = True)
+    # Debug set to False so error 500 page will show for end user
+    app.run(debug = False)
